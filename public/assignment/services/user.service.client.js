1cf66d3de261387e604464ccb542c7c7881b26d3
Raviteja Somisetty <somisetty.r@husky.neu.edu>
2016-02-17 20:28:16 -0500

    Added app,config and UserService

---
+++ public/assignment/services/user.service.client.js
@@ -0,0 +1,62 @@
+angular.factory("UserService", function ($http, $location) {
+    users = [
+        {
+            "_id": 123, "firstName": "Alice", "lastName": "Wonderland",
+            "username": "alice", "password": "alice", "roles": ["student"]
+        },
+        {
+            "_id": 234, "firstName": "Bob", "lastName": "Hope",
+            "username": "bob", "password": "bob", "roles": ["admin"]
+        },
+        {
+            "_id": 345, "firstName": "Charlie", "lastName": "Brown",
+            "username": "charlie", "password": "charlie", "roles": ["faculty"]
+        },
+        {
+            "_id": 456, "firstName": "Dan", "lastName": "Craig",
+            "username": "dan", "password": "dan", "roles": ["faculty", "admin"]
+        },
+        {
+            "_id": 567, "firstName": "Edward", "lastName": "Norton",
+            "username": "ed", "password": "ed", "roles": ["student"]
+        }
+    ];
+
+    var findUserByUsernameAndPassword = function (username, password, callback) {
+        for (var user in users) {
+            if (user.username == username && user.password == password)
+                callback(user);
+            else
+                callback(null);
+        }
+    };
+
+    var findAllUsers = function (callback) {
+        callback(users);
+    };
+
+    var createUser = function (user, callback) {
+        user._id = (new Date).getTime();
+        users.add(user);
+        callback(user);
+    };
+
+    var deleteUserById = function (userId, callback) {
+        for (var user in users) {
+            if (user._id == userId) {
+                users.remove(user);
+                callback(users);
+            }
+        }
+    };
+
+    var updateUser = function (userId, user, callback) {
+        for (var u in users) {
+            if (userId == u._id) {
+                u = user;
+                callback(u);
+            }
+        }
+    };
+
+});
