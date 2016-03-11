d684b4aef3baf5b8c8ca42708a8488a7ba0e69ea
Raviteja Somisetty <somisetty.r@husky.neu.edu>
2016-03-11 07:50:31 -0500

    added myReservations

--- public/project/client/views/users/myreservations.controller.js
+++ public/project/client/views/users/myreservations.controller.js
@@ -4,7 +4,22 @@
         .module("GrabACar")
         .controller("MyReservationsController", MyReservationsController);
     function MyReservationsController($scope, $location, $rootScope) {
-        $scope.instance=$rootScope.user.instance;
+        $scope.instance = $rootScope.user.instance;
+
+        var today = new Date();
+        if (today.valueOf() < $scope.instance.pickupdate.valueOf())
+            $scope.canBeCancelled = true;
+        else {
+            $scope.canBeCancelled = false;
+            $scope.CancellationInfo="You may not make any changes to this reservation";
+        }
+
+        $scope.cancel=function(){
+            $rootScope.user.instance=null;
+            $scope.canBeCancelled=false;
+            $scope.CancellationInfo="Your reservation has been cancelled successfully";
+        }
+
     }
 })();
 
