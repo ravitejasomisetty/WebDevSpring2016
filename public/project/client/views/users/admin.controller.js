a1602c098d22c3494778131155c6386da309cb1c
Raviteja Somisetty <somisetty.r@husky.neu.edu>
2016-03-11 21:30:37 -0500

    Teller approve/decline

--- public/project/client/views/users/admin.controller.js
+++ public/project/client/views/users/admin.controller.js
@@ -8,17 +8,32 @@
             "birthdate": "03/23/1992",
             "fullName": "Alice Wonderland",
             "licenseNumber": "ADPHHSPE12",
-            "licenseCountry": "United States"
+            "licenseCountry": "United States",
+            "status":"Waiting for approval"
         }, {
             "birthdate": "03/23/1992",
             "fullName": "Alice Wonderland",
             "licenseNumber": "ADPHHSPE12",
-            "licenseCountry": "United States"
+            "licenseCountry": "United States",
+            "status":"Approved"
         }, {
             "birthdate": "03/23/1992",
             "fullName": "Alice Wonderland",
             "licenseNumber": "ADPHHSPE12",
-            "licenseCountry": "United States"
+            "licenseCountry": "United States",
+            "status":"Declined"
         }];
+
+        $scope.approve=function(user){
+            user.status="Approved";
+            $scope.user.approved=true;
+            $scope.user.declined=false;
+        }
+
+        $scope.decline=function(user){
+            user.status="Declined";
+            $scope.user.declined=true;
+            $scope.user.approved=false;
+        }
     }
 })();
