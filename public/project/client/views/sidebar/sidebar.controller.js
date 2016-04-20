3a364631bf4bbaca1bac2d66edbc33c3984c4e0e
Raviteja Somisetty <somisetty.r@husky.neu.edu>
2016-04-19 20:23:29 -0400

    Remove sidebar

--- public/project/client/views/sidebar/sidebar.controller.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    'use strict';
-    angular
-        .module("GrabACar")
-        .controller("SidebarController", SidebarController);
-    function SidebarController($scope,$location) {
-        $scope.$location=$location;
-    }
-})();
