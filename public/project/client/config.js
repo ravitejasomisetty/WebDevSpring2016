79a7d203ef01cf37472cbdeee9a87017bab9d60c
Raviteja Somisetty <somisetty.r@husky.neu.edu>
2016-04-19 20:52:12 -0400

    registerVehicle to teller directory

--- public/project/client/config.js
+++ public/project/client/config.js
@@ -6,7 +6,7 @@
     function configuration($routeProvider) {
         $routeProvider
             .when("/vehicle", {
-                templateUrl: "client/views/vehicle/vehicle.view.html",
+                templateUrl: "client/views/teller/vehicle.view.html",
                 controller: "VehicleController",
                 controllerAs: "model",
                 resolve: {checkTellerLoggedIn: checkTellerLoggedIn}
